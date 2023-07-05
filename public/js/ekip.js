3429f891b17c2d50bdf4c2146280128d8b13612f
OrhanGezmen <133846833+OrhanGezmen@users.noreply.github.com>
2023-07-05 21:17:52 +0300

    Add files via upload

---
+++ public/js/ekip.js
@@ -0,0 +1,6 @@
+window.addEventListener("load",()=>{
+  let card = document.getElementById("card")
+  let cardImage = document.getElementById("cardImage")
+  card.style.transition = "all 5s easy-in-out"
+  cardImage.style.transition = "all 10s easy-in-out"
+})
